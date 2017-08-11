dc4180431eb016210923d3dcdc072791bb4c4d12
Sarah Perea <sdperea1@up.edu.ph>
2017-08-11 13:50:29 +0800

    Fixed usage of getParsedBody()

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ $app = new \Slim\App;
 //require_once('../app/api/users.php');
 
 //displays all records of users
-$app->get('/api/users', function() {
+$app->get('/api/users', function($request, $response) {
 
 	require_once('../app/api/dbconnect.php');
 
@@ -22,13 +22,12 @@ $app->get('/api/users', function() {
 	}
 
 	if (isset($data)) {
-		header('Content-Type: application/json');
-		echo json_encode($data);
+		return $response->withJson($data);
 	}
 });
 
 //displays a single user
-$app->get('/api/users/{id}', function($request){
+$app->get('/api/users/{id}', function($request, $response){
 
 	require_once('../app/api/dbconnect.php');
 	$id = $request->getAttribute('id');
@@ -37,45 +36,41 @@ $app->get('/api/users/{id}', function($request){
 	$result = $mysqli->query($query);
 
 	$data[] = $result->fetch_assoc();
-	header('Content-Type: application/json');
-	echo json_encode($data);
+
+	return $response->withJson($data);
 });
 
 //creates a new record of a user
-$app->post('/api/users', function($request){
+$app->post('/api/users', function($request, $response){
 
 	require_once('../app/api/dbconnect.php');
 
 	$query = "INSERT INTO user (first_name, middle_name, last_name) VALUES (?,?,?)";
 	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("sss", $first_name, $middle_name, $last_name);
 
-	$first_name = $request->getParsedBody()['first_name'];
-	$middle_name = $request->getParsedBody()['middle_name'];
-	$last_name = $request->getParsedBody()['last_name'];
-	
+	$parsedBody = $request->getParsedBody();
+	$stmt->bind_param("sss", $parsedBody['first_name'], $parsedBody['middle_name'], $parsedBody['last_name']);
+
 	$stmt->execute();
 });
 
 //updates a record of a user
-$app->put('/api/users/{id}', function($request) {
+$app->put('/api/users/{id}', function($request, $response) {
 
 	require_once('../app/api/dbconnect.php');
 	$id = $request->getAttribute('id');
 
 	$query = "UPDATE user SET first_name = ?, middle_name = ?, last_name = ? WHERE user.user_id = $id";
 	$stmt = $mysqli->prepare($query);
-	$stmt->bind_param("sss", $first_name, $middle_name, $last_name);
 
-	$first_name = $request->getParsedBody()['first_name'];
-	$middle_name = $request->getParsedBody()['middle_name'];
-	$last_name = $request->getParsedBody()['last_name'];
+	$parsedBody = $request->getParsedBody();
+	$stmt->bind_param("sss", $parsedBody['first_name'], $parsedBody['middle_name'], $parsedBody['last_name']);
 
 	$stmt->execute();
 });
 
 //deletes a record from the database
-$app->delete('/api/users/{id}', function($request) {
+$app->delete('/api/users/{id}', function($request, $response) {
 
 	require_once('../app/api/dbconnect.php');
 	$id = $request->getAttribute('id');
