e8523485661671efb429e89b8666bf18f7df09a8
Sarah Perea <sdperea1@up.edu.ph>
2017-08-11 15:02:33 +0800

    Created classes for controllers

---
+++ app/Controllers/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class HomeController extends Controller
+{
+
+	public function index($request, $response){
+		return 'Home controller';		
+		//echo "<pre>";
+		//var_dump ($args);
+		//echo "</pre>";
+	}
+}
