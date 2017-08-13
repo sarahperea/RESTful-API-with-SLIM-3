a6ee1905a807d54ab43ec9e6ed698c7a52b53f59
Sarah Perea <sdperea1@up.edu.ph>
2017-08-13 14:15:08 +0800

    Fixed usage of beans

--- app/models/Model_User.php
+++ app/models/Model_User.php
@@ -16,21 +16,32 @@ class Model_User{
 	}
 
 	public function retrieve(){
-		return \R::getAll( "SELECT * FROM user ORDER BY user_id" );
+		return \R::find('users');
 	}
 
 	public function create($parsedBody){
-		\R::exec( "INSERT INTO user (first_name, middle_name, last_name) VALUES (:fn,:mn,:ln)",
-			array(':fn' => $parsedBody['first_name'], ':mn' => $parsedBody['middle_name'], ':ln' => $parsedBody['last_name']) );
+		$user = \R::dispense([
+			'_type' => 'users',
+			'firstname' => $parsedBody['first_name'],
+			'middlename' => $parsedBody['middle_name'],
+			'lastname' => $parsedBody['last_name'],
+		]);
+		\R::store($user);
+		return $user;
 	}
 
 	public function update($parsedBody, $id){
-		\R::exec( "UPDATE user SET first_name = :fn, middle_name = :mn, last_name = :ln WHERE user.user_id = :id",
-			array(':id' => $id, ':fn' => $parsedBody['first_name'], ':mn' => $parsedBody['middle_name'], ':ln' => $parsedBody['last_name']) );
+		$user = \R::load('users',$id);
+		$user->firstname = $parsedBody['first_name'];
+		$user->middlename = $parsedBody['middle_name'];
+		$user->lastname = $parsedBody['last_name'];
+		\R::store($user);
+		return $user;
 	}
 
 	public function delete($id){
-		\R::exec( "DELETE FROM user WHERE user.user_id = :id", array(':id' => $id) );
+		$user = \R::load('users',$id);
+		\R::trash( $user);
 	}
 }
 
